60ba1d603b275e79e29083475baf21d8790fffb8
Youlounn <yohann.rialet28@gmail.com>
2018-11-19 21:47:53 +0100

    intersection entre l'arc et le noeud
    l'arc ne va plus au centre du noeud mais au bord (pas vraiment parfait)

--- TP2/app/src/main/java/com/example/yoyob/tp2/DrawableGraph.java
+++ TP2/app/src/main/java/com/example/yoyob/tp2/DrawableGraph.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.graphics.PixelFormat;
 import android.graphics.RectF;
+import android.graphics.Region;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -93,7 +94,6 @@ public class DrawableGraph extends Drawable {
         arcPaint.setStrokeJoin(Paint.Join.ROUND);
         arcPaint.setStrokeWidth(arc.getWidth());
         Log.d("updatePath",arc.getPathMidX()+" / "+arc.getPathMidY());
-        canvas.drawPath(arc.getPath(),arcPaint);
         arcNamePaint.setColor(arc.getColor());
         arcNamePaint.setTextAlign(Paint.Align.CENTER);
         arcNamePaint.setTextSize(arc.getTextSize());
@@ -101,14 +101,48 @@ public class DrawableGraph extends Drawable {
         float[] tangent = {0f, 0f};
         PathMeasure pm = new PathMeasure(arc.getPath(), false);
         pm.getPosTan(pm.getLength() * 0.5f, midPoint, tangent);
-        Log.d("xxxxxmid0",""+midPoint[0]);
-        Log.d("xxxxxmid1",""+midPoint[1]);
-        Log.d("xxxxxtan0",""+tangent[0]);
-        Log.d("xxxxxtan0",""+tangent[1]);
         if(arc.getNodeDep() == arc.getNodeArr()){
             canvas.drawText(arc.getName(), arc.getPathMidX(), arc.getPathMidY(), arcNamePaint);
         }else{
             canvas.drawText(arc.getName(), midPoint[0], midPoint[1], arcNamePaint);
+        }
+        Path path = new Path();
+        RectF rectFDep = new RectF(arc.getNodeDep().getX(),arc.getNodeDep().getY(),arc.getNodeDep().getX()+arc.getNodeDep().getWidth(),arc.getNodeDep().getY()+arc.getNodeDep().getHeight());
+        path.addRoundRect(rectFDep, 0, 0, Path.Direction.CW);
+        path.computeBounds(rectFDep, true);
+        Region regionDep = new Region();
+        regionDep.setPath(path, new Region(arc.getNodeDep().getX(),arc.getNodeDep().getY(),arc.getNodeDep().getX()+arc.getNodeDep().getWidth(),arc.getNodeDep().getY()+arc.getNodeDep().getHeight()));
+        pm = new PathMeasure(arc.getPath(), false);
+        float[] pointDep = {0f, 0f};
+        float[] pointArr = {0f, 0f};
+        pm.getPosTan(pm.getLength() * 0.5f, pointDep, null);
+        boolean d = regionDep.contains((int) pointDep[0], (int) pointDep[1]);
+        if(arc.getNodeArr() != null){
+            while(!d){
+                pointDep[0] = (pointDep[0]+arc.getNodeDep().getX()+(arc.getNodeDep().getWidth() / 2))/2;
+                pointDep[1] = (pointDep[1]+arc.getNodeDep().getY()+(arc.getNodeDep().getHeight() / 2))/2;
+                d = regionDep.contains((int) pointDep[0], (int) pointDep[1]);
+            }
+            RectF rectFArr = new RectF(arc.getNodeArr().getX(),arc.getNodeArr().getY(),arc.getNodeArr().getX()+arc.getNodeArr().getWidth(),arc.getNodeArr().getY()+arc.getNodeArr().getHeight());
+            path.addRoundRect(rectFArr, 0, 0, Path.Direction.CW);
+            path.computeBounds(rectFArr, true);
+            Region regionArr = new Region();
+            regionArr.setPath(path, new Region(arc.getNodeArr().getX(),arc.getNodeArr().getY(),arc.getNodeArr().getX()+arc.getNodeArr().getWidth(),arc.getNodeArr().getY()+arc.getNodeArr().getHeight()));
+            pm.getPosTan(pm.getLength() * 0.5f, pointArr, null);
+            d = regionArr.contains((int) pointArr[0], (int) pointArr[1]);
+            while(!d){
+                pointArr[0] = (pointArr[0]+arc.getNodeArr().getX()+(arc.getNodeArr().getWidth() / 2))/2;
+                pointArr[1] = (pointArr[1]+arc.getNodeArr().getY()+(arc.getNodeArr().getHeight() / 2))/2;
+                d = regionArr.contains((int) pointArr[0], (int) pointArr[1]);
+            }
+        }
+        if(arc.getNodeArr() != null){
+            Path edgePath = new Path();
+            edgePath.moveTo(pointDep[0],pointDep[1]);
+            edgePath.quadTo(arc.getPathMidX(),arc.getPathMidY(),pointArr[0],pointArr[1]);
+            canvas.drawPath(edgePath,arcPaint);
+        }else{
+            canvas.drawPath(arc.getPath(),arcPaint);
         }
     }
 
